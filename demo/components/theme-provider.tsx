59624face67af704668e28bc7b70d4f5d43bf6c2
sachin <sachncs@gmail.com>
2026-08-26 14:27:44 +0530

    feat(demo): add theme provider (dark by default)

---
+++ demo/components/theme-provider.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import * as React from 'react';
+
+type Theme = 'dark';
+
+type ThemeContextValue = {
+  theme: Theme;
+  setTheme: (t: Theme) => void;
+};
+
+const ThemeContext = React.createContext<ThemeContextValue | undefined>(
+  undefined,
+);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = React.useState<Theme>('dark');
+
+  React.useEffect(() => {
+    document.documentElement.classList.add('dark');
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const ctx = React.useContext(ThemeContext);
+  if (!ctx) throw new Error('useTheme must be used inside <ThemeProvider>');
+  return ctx;
+}
